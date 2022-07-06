05231cff287970698374d36c8db8824f6f0edb63
ahmed dreed <98590782+ahmeddreed@users.noreply.github.com>
2022-07-06 11:54:19 +0300

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,7 +18,7 @@
 
   <!-- Vendor CSS Files -->
   <link href="{{ asset("assets/vendor/aos/aos.css") }}" rel="stylesheet">
-  <link href="{{ asset("assets/vendor/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="{{ asset("assets/vendor/bootstrap-icons/bootstrap-icons.css") }}" rel="stylesheet">
   <link href="{{ asset("assets/vendor/boxicons/css/boxicons.min.css") }}" rel="stylesheet">
   <link href="{{ asset("assets/vendor/glightbox/css/glightbox.min.css") }}" rel="stylesheet">
